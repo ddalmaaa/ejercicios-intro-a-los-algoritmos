58321e8f8bfebe15676b099b5c4bbac1412e54a9
Dalma Vogarin <dalmanereavogarin@gmail.com>
2026-05-07 19:33:07 -0300

    malisimo

---
+++ Fecha.java
@@ -0,0 +1,54 @@
+
+/**
+ * Write a description of class fecha here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Fecha
+{
+    // instance variables - replace the example below with your own
+    private int año;
+    private int mes;
+    private int anho;
+
+    /**
+     * Constructor for objects of class fecha
+     */
+    public Fecha(int dia, int mes, int anho)
+    {
+          dia = dias;
+          mes = meses;
+          anho = años;
+          
+          assert anhos >= 1582: "no es del calendario";
+
+
+    }
+     
+    //devuelve la cantidad de d´ıas que tiene el mes dado en el a˜no indicado
+    private int cantidadDias(int mes, int anho){
+        
+    }
+    //verifica si la fecha es v´alida.
+    private boolean esBisiesto(int anho){
+        
+    }
+
+    //verifica si la fecha es v´alida.
+    private boolean fechaValida(int dia, int mes, int anho){
+    
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        
+    }
+}
